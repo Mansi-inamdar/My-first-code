99533f4a333c78957ce749fbfb11db5d9a0f9b33
Mansi-inamdar <inamdarmansi958@gmail.com>
2025-11-29 19:58:26 +0530

    Add star printing program in C

---
+++ star printing.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+     int main (){
+     	
+     	  int rows,i,j;
+     	  
+     	  printf("enter the no of rows:");
+     	  scanf("%d",&rows);
+     	  
+     	  for(i=1;i<=rows;i++){
+     	  	printf("*");
+     	  	
+		   }
+     	return 0;
+	 }
