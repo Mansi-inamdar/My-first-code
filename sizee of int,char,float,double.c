88e74e178270a7e1b5b60e25db4cc6b1f5b3a633
Mansi-inamdar <inamdarmansi958@gmail.com>
2025-11-30 21:26:52 +0530

    Add program to find sizes of data types
    This program prints the size of different data types: char, int, float, and double.

---
+++ sizee of int,char,float,double.c
@@ -0,0 +1,13 @@
+//finding size of int,float,double,char........
+#include<stdio.h>
+void main(){
+	char c;
+	int i;
+	float f;
+	double d;
+	printf("size of char=%zu bytes\n",sizeof(c));
+	printf("size of int=%zu bytes\n",sizeof(i));
+	printf("size of float=%zu bytes\n",sizeof(f));
+	printf("size of double=%zu bytes\n",sizeof(d));
+	
+}
