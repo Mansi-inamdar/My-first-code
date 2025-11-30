2d2bcaf9880ea22b4f3909b6715478e51c6f55c9
Mansi-inamdar <inamdarmansi958@gmail.com>
2025-11-30 20:56:31 +0530

    Create multiplication of two flaot numbers.c

---
+++ multiplication of two flaot numbers.c
@@ -0,0 +1,10 @@
+//muitiply two float values
+#include<stdio.h>
+void main(){
+	float num1,num2,multiplication;
+	printf("enter num1,num2:");
+	scanf("%f %f",&num1,&num2);
+	multiplication=num1*num2;
+	printf("multiplication=%.2f",multiplication);
+	
+}
